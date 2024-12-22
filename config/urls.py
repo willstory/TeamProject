c834b83a7f4758f57d36a73038507eb4abddb0ec
SS <yo2189@naver.com>
2024-12-22 19:49:00 +0900

    team project basic structure

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for congif project.
+URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import settings
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("",views.Homeview.as_view(), name='index')
 ]
