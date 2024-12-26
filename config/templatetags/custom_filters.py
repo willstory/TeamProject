17211695f37c51a9b986e56c6e34038e623365e7
bwlee0425 <bwlee0425@gmail.com>
2024-12-26 15:03:13 +0900

    회원가입디자인

---
+++ config/templatetags/custom_filters.py
@@ -0,0 +1,8 @@
+# yourapp/templatetags/custom_filters.py
+from django import template
+
+register = template.Library()
+
+@register.filter
+def add_class(field, css_class):
+    return field.as_widget(attrs={"class": css_class})
