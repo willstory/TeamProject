842e5464dc754b51ae2468a3dfee288c10d4d71f
matthew.shim <matthew.shim@gmail.com>
2025-02-06 01:28:16 +0900

    문제 리스트 및 문제 리스트 선택 결과 페이지

--- academy/urls.py
+++ academy/urls.py
@@ -6,7 +6,7 @@ app_name = 'academy'
 
 urlpatterns = [
     path('academy_list/', views.academy_list, name='academy_list'),
-    #path('<int:academy_id>/', views.academy_detail, name='academy_detail'),
+    path('academy_list_result/', views.academy_list_result, name='academy_list_result'),
     #path('create/', views.academy_create, name='academy_create'),
     #path('<int:academy_id>/edit/', views.academy_edit, name='academy_edit'),
     #path('<int:academy_id>/delete/', views.academy_delete, name='academy_delete'),
